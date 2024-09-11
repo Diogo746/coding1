d6ed9041870d41dfc7b439a31aa29ae2b9cad62f
Diogo <diogo.barbosa@edu.pe.senac.br>
2024-09-11 16:15:04 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,1 +1,19 @@
+const numero1 = 20;
 
+const numero2 = 10;
+
+const soma = numero1 + numero2;
+
+console.log("A some é: " + soma);
+
+const sub = numero1 - numero2;
+
+console.log("A subtração é: " + sub);
+
+const mult = numero1 * numero2;
+
+console.log("A multiplicação é: " + mult);
+
+const div = numero1 / numero2;
+
+console.log("A divisão é: " + div);
