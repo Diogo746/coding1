9cf5c855e2438fc124fe6941140978178578268e
Diogo <diogo.barbosa@edu.pe.senac.br>
2024-09-18 16:10:05 -0300

    Update AtividadeTiposPrimitivos.js

--- AtividadeTiposPrimitivos.js
+++ AtividadeTiposPrimitivos.js
@@ -1,1 +1,36 @@
+// Nome e idade
 
+
+let nome = "Diogo";
+console.log("Meu nome é " + nome);
+let idade = "31";
+console.log("Tenho " + idade +" anos");
+
+// Cidade
+
+let nome = "Diogo";
+let cidade = "Recife";
+
+console.log(`Meu nome é ${nome} e sou de ${cidade}`);
+
+// Operações básicas
+
+const numero1 = 30;
+const numero2 = 20;
+const soma = numero1 + numero2;
+console.log(`O resultado da soma é: ${soma}`);
+const sub = numero1 - numero2;
+console.log(`O resultado da subtração é: ${sub}`);
+const mult = numero1 * numero2;
+console.log(`O resultado da multiplicação é: ${mult}`);
+const div = numero1 / numero2;
+console.log(`O resultado da divisão é: ${div}`);
+
+
+// Média
+
+const nota1 = 9;
+const nota2 = 7;
+const nota3 = 5;
+const media = (nota1 + nota2+ nota3)/3;
+console.log(`O resultado da média é ${media}`);
